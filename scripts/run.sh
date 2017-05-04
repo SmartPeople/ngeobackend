3a9eaadf1eb28405535513ffcb5cc55052f95d47
smartpeople <semeyon@speople.pro>
2017-05-04 17:30:25 +0600

    Add vm start/stop to scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,6 +7,7 @@ docker rm -v $(docker ps -a -q -f status=exited)
 docker rmi $(docker images -f \"dangling=true\" -q)
 docker run -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker --rm martin/docker-cleanup-volumes
 
+docker-machine run docker-sandbox
 docker-compose -f docker-compose.yml stop
 docker-compose -f docker-compose.yml up --build -d
 
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,2 +1,3 @@
 eval $(docker-machine env docker-sandbox)
 docker-compose -f docker-compose.yml stop
+docker-machine stop docker-sandbox
