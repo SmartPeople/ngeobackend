225c2f428da4389f25c3d5e164bd99d7bd093656
smartpeople <semeyon@speople.pro>
2017-05-04 18:22:23 +0600

    scripts/dbbackup.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,4 @@
+docker-machine start docker-sandbox
 eval $(docker-machine env docker-sandbox)
 
 docker stop $(docker ps -a -q)
@@ -7,7 +8,7 @@ docker rm -v $(docker ps -a -q -f status=exited)
 docker rmi $(docker images -f \"dangling=true\" -q)
 docker run -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker --rm martin/docker-cleanup-volumes
 
-docker-machine run docker-sandbox
+
 docker-compose -f docker-compose.yml stop
 docker-compose -f docker-compose.yml up --build -d
 
