5a2a36e40e2ff0e29845f175bd292cdc529e5f3e
smartpeople <semeyon@speople.pro>
2017-05-04 16:26:10 +0600

    New devops scripts and small code cleanup

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,5 @@
+eval $(docker-machine env docker-sandbox)
+
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 
@@ -5,5 +7,7 @@ docker rm -v $(docker ps -a -q -f status=exited)
 docker rmi $(docker images -f \"dangling=true\" -q)
 docker run -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker --rm martin/docker-cleanup-volumes
 
-docker-compose -f docker-compouse.yml build
-docker-compose -f docker-compouse.yml up
+docker-compose -f docker-compose.yml stop
+docker-compose -f docker-compose.yml up --build -d
+
+docker-machine ls
---
+++ scripts/stop.sh
@@ -0,0 +1,2 @@
+eval $(docker-machine env docker-sandbox)
+docker-compose -f docker-compose.yml stop
