f12ad5840e3cc367c72796c99d609cfca658feed
smartpeople <semeyon@speople.pro>
2017-05-30 17:12:35 +0600

    Filtering points on the heatmap page

--- web/controllers/pages_controller.ex
+++ web/controllers/pages_controller.ex
@@ -12,7 +12,32 @@ defmodule NGEOBackend.PagesController do
     |> assign(:points, NGEOBackend.Event.allPositions())
     |> render("grouped_points.html")
   end
-  
+
+  def heatmap_points(conn, %{"filter" => %{"dt_start" => dt_start, "dt_end" => dt_end, "user_id" => user_id}}) do
+    start_dt = 
+      NaiveDateTime.from_erl!({
+        {
+          String.to_integer(dt_start["year"]), 
+          String.to_integer(dt_start["month"]),
+          String.to_integer(dt_start["day"])
+        }, 
+        {00, 00, 00}
+      });
+
+    end_dt   = 
+      NaiveDateTime.from_erl!({
+        {
+          String.to_integer(dt_end["year"]), 
+          String.to_integer(dt_end["month"]),
+          String.to_integer(dt_end["day"])
+        }, 
+        {00, 00, 00}
+      });
+    conn
+    |> assign(:points, NGEOBackend.Event.filteredPositions(start_dt, end_dt, user_id))
+    |> render("heatmap_points.html")
+  end
+
   def heatmap_points(conn, _params) do
     conn
     |> assign(:points, NGEOBackend.Event.allPositions())
--- web/views/pages_view.ex
+++ web/views/pages_view.ex
@@ -5,4 +5,16 @@ defmodule NGEOBackend.PagesView do
     Enum.filter(points, &(&1.data["type"] == 0))
   end
 
+  def numberOfPoints(points) do
+    Enum.count(points, &(&1.data["type"] == 0))
+  end
+
+  def numberOfEvents(points) do
+    Enum.count(points)
+  end
+
+  def allUsers do
+    Enum.map(NGEOBackend.User.all(), fn(u) -> {u.email, u.id} end)
+  end
+
 end
